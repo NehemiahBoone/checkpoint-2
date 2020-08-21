6e776a472e570afd29476bb3ad20549d15dd9f60
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-08-21 13:34:23 -0600

    +1 shovel button works

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
 let cheese = 0;
 let updateCheese = document.getElementById("cheeseCount")
+let updateShovels = document.getElementById("shovels")
+let clickUpgrades = {
+  shovels: {
+    price: 50,
+    quantity: 0,
+    multiplier: 1
+  }
+}
+
+let automaticUpgrades = {
+  cheeseMouse: {
+    price: 500,
+    quantity: 0,
+    multiplier: 2
+  }
+}
 
 
 function mine() {
@@ -12,4 +28,16 @@ function update() {
   updateCheese.innerHTML = `
   ${cheese}
   `
+}
+
+function buyShovel() {
+  if (cheese >= clickUpgrades.shovels.price) {
+    clickUpgrades.shovels.quantity++
+    cheese -= clickUpgrades.shovels.price
+    updateShovels.innerHTML = `
+    ${clickUpgrades.shovels.quantity} Shovels
+    `
+    update()
+    console.log(clickUpgrades.shovels.quantity)
+  }
 }
